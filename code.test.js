3203018b6b69d53b39917997293a17e4deb5fe2e
Dhruv8806 <123929419+Dhruv8806@users.noreply.github.com>
2023-10-19 16:49:05 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -5,7 +5,9 @@ eval(fs.readFileSync('code.js')+'');
 
 const test =
     jsc.forall("array nat", function(arr) {
-        var count = Sorted(arr); 
-        return count >= 0;
+        var a1 = JSON.parse(JSON.stringify(arr));
+        var a2 = JSON.parse(JSON.stringify(arr));
+        var count = permutationSort(a1);
+        return count >= 0 && JSON.stringify(a1) == JSON.stringify(a2.sort(function(a, b) { return a - b; }));
     });
 jsc.assert(test);
